20429b17a69d1190f2c9365c4c9d15df727d0174
krshivam <kumarshivam675@gmail.com>
2017-09-14 05:07:55 +0530

    exploration code added

---
+++ code/read_json.py
@@ -0,0 +1,24 @@
+import json
+
+path = '/home/shivam/coursework/user engagement/User-Engagement-Video/logdumps_organisationX/'
+
+count = 0
+with open(path + 'logdump_2000000.json') as json_data:
+    d = json.load(json_data)
+
+# count = 1
+# for feature in d:
+#     count += 1
+#     if count > 1000:
+#         break
+#     with open('data2.txt', 'a') as outfile:
+#         json.dump(feature, outfile)
+
+count = 1
+for feature in d:
+    count += 1
+    with open('data_user_fb62884cc3fa8a4bfb36535fa628acff22830025acb5ebe31e1ef5ef.txt', 'a') as outfile:
+        if feature['email_id'] == 'fb62884cc3fa8a4bfb36535fa628acff22830025acb5ebe31e1ef5ef':
+            json.dump(feature, outfile)
+
+
---
+++ code/savetocsv.py
@@ -0,0 +1,10 @@
+import csv
+
+
+def save(file, timestamp, pid, client_message, offset, timestamp_2):
+
+    hello = [[file, timestamp, pid, client_message, offset, timestamp_2]]
+
+    with open('user_all_dumps.csv', 'a') as testfile:     # append it data to the csv file
+        csv_writer = csv.writer(testfile)
+        csv_writer.writerow(hello[0])
