0878b890dbf1994e5797ab9b49ca58ac02eaa671
krshivam <kumarshivam675@gmail.com>
2017-09-20 04:17:01 +0530

    unique video changed

--- code/expt/uniquevideo.py
+++ code/expt/uniquevideo.py
@@ -5,15 +5,18 @@ import datetime
 
 path = '/home/trisha/Desktop/Acad/Semester9/PE/User-Engagement-Video/code/'
 file = 'result_fb62884cc3fa8a4bfb36535fa628acff22830025acb5ebe31e1ef5ef.json'
+
+
 # file ='result_logdump_400000.json'
 
 def extract_week_number(raw_date):
-	year = raw_date[:4]
-	month = raw_date[5:7]
-	day = raw_date[8:]
-	dt = datetime.date(int(year), int(month), int(day))
-	wk = dt.isocalendar()[1]
-	return wk
+    year = raw_date[:4]
+    month = raw_date[5:7]
+    day = raw_date[8:]
+    dt = datetime.date(int(year), int(month), int(day))
+    wk = dt.isocalendar()[1]
+    return wk
+
 
 '''
 with open(path + file, 'r') as data_file:
@@ -24,14 +27,17 @@ with open(path + file, 'r') as data_file:
 '''
 
 store_week_number = []
+week_unique_video = {}
 with open(path + file, 'r') as data_file:
-	for line in data_file.readlines():
-        	data = json.loads(line)
-		raw_Video_Id = data['Video_Id']
-            	raw_date = data['date']
-	    	week_number = extract_week_number(raw_date)
-		store_week_number.append(week_number)
-		print week_number, raw_Video_Id
-	print min(store_week_number), len(store_week_number), max(store_week_number)
-		
-
+    for line in data_file.readlines():
+        data = json.loads(line)
+        raw_Video_Id = data['Video_Id']
+        raw_date = data['date']
+        week_number = extract_week_number(raw_date)
+        if week_number in week_unique_video:
+            week_unique_video[week_number].append(raw_Video_Id)
+        else:
+            week_unique_video[week_number] = [raw_Video_Id]
+        store_week_number.append(week_number)
+        print week_number, raw_Video_Id
+    print min(store_week_number), len(store_week_number), max(store_week_number)
