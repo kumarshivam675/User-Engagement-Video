fb52c60482c8569e370dc3e80af7f6c6f4f8e4fe
TrishaMittal <Trisha.Mittal@iiitb.org>
2017-11-25 17:18:06 +0530

    Login

--- code/combine_features/combine.py
+++ code/combine_features/combine.py
@@ -7,11 +7,13 @@ sys.path.insert(0, project_folder + 'code/extract_duration')
 sys.path.insert(0, project_folder + 'code/extract_phrasecloud')
 sys.path.insert(0, project_folder + 'code/extract_mmtoc')
 sys.path.insert(0, project_folder + 'code/extract_query')
+sys.path.insert(0, project_folder + 'code/extract_login')
 
 import duration_ver3
 import phrasecloud
 import mmtoc
 import query
+import login
 
 
 def sort_values_on_keys(dictionary):
@@ -39,8 +41,8 @@ def multiply_scalar_vector(wt, vec):
     return [i * wt for i in vec]
 
 
-def add_lists(l1, l2, l3, l4):
-    return map(sum, zip(l1, l2, l3, l4))
+def add_lists(l1, l2, l3, l4, l5):
+    return map(sum, zip(l1, l2, l3, l4, l5))
 
 
 def save(email, user_cluster, data, filename):
@@ -55,12 +57,13 @@ def save(email, user_cluster, data, filename):
         print "key error", email
 
 
-def combine_vectors(mmtoc_wt, phrase_wt, duration_wt, query_wt):
+def combine_vectors(mmtoc_wt, phrase_wt, duration_wt, query_wt, login_wt):
     path = project_folder + "code/cluster_data/"
     mmtoc_dict = mmtoc.extract_mmtoc_clicks()
     phrasecloud_dict = phrasecloud.extract_phrasecloud_clicks()
     duration_dict = duration_ver3.create_video_log()
     query_dict = query.extract_query_clicks()
+    user_login = login.extract_login_clicks()
     user_cluster = extract_ground_truth.ground_truth()
     count = 0
 
@@ -73,6 +76,7 @@ def combine_vectors(mmtoc_wt, phrase_wt, duration_wt, query_wt):
         phrasecloud_vector = extract_vectors(phrasecloud_dict[user])
         duration_vector = extract_vectors(duration_dict[user])
         search_query_vector = extract_vectors(query_dict[user])
+        login_vector = extract_vectors(user_login[user])
 
         # print "vector extraction done"
 
@@ -80,10 +84,10 @@ def combine_vectors(mmtoc_wt, phrase_wt, duration_wt, query_wt):
         weighted_f2 = multiply_scalar_vector(phrase_wt, phrasecloud_vector)
         weighted_f3 = multiply_scalar_vector(duration_wt, duration_vector)
         weighted_f4 = multiply_scalar_vector(query_wt, search_query_vector)
-
+        weighted_f5 = multiply_scalar_vector(login_wt, login_vector)
         # print "vector multiplication done"
 
-        feature_vector = add_lists(weighted_f1, weighted_f2, weighted_f3, weighted_f4)
+        feature_vector = add_lists(weighted_f1, weighted_f2, weighted_f3, weighted_f4, weighted_f5)
 
         # print "beginning to save in the csv"
         save(user, user_cluster, feature_vector, path + "feature_vectors.csv")
@@ -91,8 +95,9 @@ def combine_vectors(mmtoc_wt, phrase_wt, duration_wt, query_wt):
         save(user, user_cluster, multiply_scalar_vector(1, mmtoc_vector), path + "mmtoc_vectors.csv")
         save(user, user_cluster, multiply_scalar_vector(1, duration_vector), path + "duration_vectors.csv")
         save(user, user_cluster, multiply_scalar_vector(1, phrasecloud_vector), path + "phrasecloud_vectors.csv")
-        save(user, user_cluster, multiply_scalar_vector(1, phrasecloud_vector), path + "search_query_vectors.csv")
+        save(user, user_cluster, multiply_scalar_vector(1, search_query_vector), path + "search_query_vectors.csv")
+        save(user, user_cluster, multiply_scalar_vector(1, login_vector), path + "login_vectors.csv")
 
 
-combine_vectors(0.33, 0.33, 0, 0.33)
+combine_vectors(0.25, 0.25, 0, 0.25, 0.25)
 
---
+++ code/extract_login/login.py
@@ -0,0 +1,76 @@
+import json
+import csv
+import os
+import datetime
+import matplotlib.pyplot as plt
+from config import project_folder
+
+# project_folder = "/home/shivam/coursework/user engagement/User-Engagement-Video/"
+path = project_folder + "code/userdata/"
+
+
+def extract_week_number(raw_date):
+    year = raw_date[:4]
+    month = raw_date[5:7]
+    day = raw_date[8:]
+    dt = datetime.date(int(year), int(month), int(day))
+    wk = dt.isocalendar()[1]
+    return wk
+
+def create_query_dictionary(min_week, max_week):
+    query_searches = {}
+    for i in range(min_week, max_week + 1):
+        query_searches[i] = 0.0
+    return query_searches
+
+def extract_login_clicks():
+    print "query"
+    user_log = {}
+
+    count = 0
+    for file in os.listdir(path):
+        user_clicks = create_query_dictionary(18, 37)
+        count += 1
+
+        if file != ".gitignore":
+            with open(path + file, 'r') as data_file:
+                date = []
+                for line in data_file.readlines():
+                    data = json.loads(line)
+                    if data['user_action'] != None:
+                        date.append(data['date'])
+
+                        '''
+                        week_number = extract_week_number(data['date'])
+                        if week_number in query_clicks:
+                            query_clicks[week_number] += 1
+                        else:
+                            query_clicks[week_number] =+
+                             1.0
+                        '''
+                date1 = list(set(date))
+                for i in date1:
+                    if extract_week_number(i) in user_clicks:
+                        user_clicks[extract_week_number(i)] += 1
+                    else:
+                        user_clicks[extract_week_number(i)] = 1.0
+
+
+                user_log[file.split("_")[1].split(".")[0]] = user_clicks
+
+    return user_log
+
+
+def queryplot_behaviour():
+    query_clicks = extract_query_clicks()
+
+    for user in query_clicks:
+        keys = query_clicks[user].keys()
+        values = query_clicks[user].values()
+        Z1 = sorted(keys)
+        Z2 = [x for _, x in sorted(zip(keys, values))]
+        plt.plot(Z1, Z2)
+    plt.savefig("query.png")
+    plt.show()
+
+#queryplot_behaviour()
